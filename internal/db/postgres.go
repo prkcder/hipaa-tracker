b488ddd59e04d5050c1fe0f8ce7a0bfad3d72718
john <jhnprk12@gmail.com>
2025-08-05 14:30:33 -0700

    update to render database use

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -1,37 +1,40 @@
 package db
 
-
 import (
 	"database/sql"
 	"fmt"
 	"os"
 
 	_ "github.com/lib/pq"
-	
 )
 
 func Connect() (*sql.DB, error) {
 
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-        os.Getenv("DB_HOST"),
-        os.Getenv("DB_PORT"),
-        os.Getenv("DB_USER"),
-        os.Getenv("DB_PASSWORD"),
-        os.Getenv("DB_NAME"),
-	)
+	// dsn := fmt.Sprintf(
+	// 	"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	//     os.Getenv("DB_HOST"),
+	//     os.Getenv("DB_PORT"),
+	//     os.Getenv("DB_USER"),
+	//     os.Getenv("DB_PASSWORD"),
+	//     os.Getenv("DB_NAME"),
+	// )
+
+	// Using Render db
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		return nil, fmt.Errorf("DATABASE_URL not set")
+	}
 
 	db, err := sql.Open("postgres", dsn)
 
 	if err != nil {
-        return nil, err
-    }
+		return nil, err
+	}
 
-    err = db.Ping()
-    
-    if err != nil {
-        return nil, err
-    }
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
 
-    return db, nil
+	return db, nil
 }
